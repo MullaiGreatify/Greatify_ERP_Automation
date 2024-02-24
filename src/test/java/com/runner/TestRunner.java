2b40be47c637341f56e29b3ab15ce912dbd8509a
MullaiGreatify <mullai.m@greatify.ai>
2024-02-24 11:22:38 +0530

    Fee Management

--- src/test/java/com/runner/TestRunner.java
+++ src/test/java/com/runner/TestRunner.java
@@ -7,26 +7,30 @@ import java.io.IOException;
 import org.junit.AfterClass;
 import org.junit.runner.RunWith;
 
+import com.base.BaseClass;
 import com.pages.EmailReport;
+import org.junit.jupiter.api.Test;
 
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import io.cucumber.junit.CucumberOptions.SnippetType;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(tags = "@StudentInformation", stepNotifications = true, dryRun = (false), monochrome = true, snippets = SnippetType.CAMELCASE, plugin = {
+@CucumberOptions(tags = "@TimetableConfiguration", stepNotifications = true, dryRun = (false), monochrome = true, snippets = SnippetType.CAMELCASE, plugin = {
 		"pretty", "html:target//output.html",
 		"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:" }, features = "src/test/resources", glue = "com.stepdefinition")
-public class TestRunner {
+public class TestRunner extends BaseClass{
 
 	@AfterClass
 	public static void afterClass() throws FileNotFoundException, IOException {
 
 		EmailReport.main(null);
 		System.clearProperty("javax.net.debug");
+//		closeAllWind();
 	}
 
 }
+
 
 /*
  * Allure Report Plugin: io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm Extent
@@ -35,9 +39,4 @@ public class TestRunner {
  * Report Plugin:
  * com.cucumber.listener.ExtentCucumberFormatter:target/cucumber-reports/report.
  * html target/cucumber-reports/report.html
- * 
- * 
- * 
- * 
- * 
  */
