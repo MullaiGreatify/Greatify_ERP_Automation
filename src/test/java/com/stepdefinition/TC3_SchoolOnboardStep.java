426ae8c1b809ea02aa2c025f85b7ba8208c9fefd
MullaiGreatify <mullai.m@greatify.ai>
2024-03-07 10:29:08 +0530

    new

--- src/test/java/com/stepdefinition/TC3_SchoolOnboardStep.java
+++ src/test/java/com/stepdefinition/TC3_SchoolOnboardStep.java
@@ -27,27 +27,27 @@ public class TC3_SchoolOnboardStep extends BaseClass {
 		implicitwait(30);
 		maxWindow();
 
-		pom.getSchoolOnboardPage().LogintoSuperAdminDashboard();
+//		pom.getSchoolOnboardPage().LogintoSuperAdminDashboard();
 
 	}
 
 	@When("the user navigates to the {string} page in the Super Admin Dashboard")
 	public void theUserNavigatesToThePageInTheSuperAdminDashboard(String string) {
 
-		pom.getSchoolOnboardPage().NavigatetoManageSchoolsPage();
+//		pom.getSchoolOnboardPage().NavigatetoManageSchoolsPage();
 	}
 
 	@When("the user generates a new School Profile invite from the Super Admin Dashboard")
 	public void theUserGeneratesANewSchoolProfileInviteFromTheSuperAdminDashboard()
 			throws FileNotFoundException, IOException, CsvException {
 
-		pom.getSchoolOnboardPage().GeneratesNewSchoolProfileInvite();
+//		pom.getSchoolOnboardPage().GeneratesNewSchoolProfileInvite();
 	}
 
 	@Then("the user should verify the {string} popup message in the Super Admin Dashboard.")
 	public void theUserShouldVerifyThePopupMessageInTheSuperAdminDashboard(String string) throws InterruptedException {
 
-		pom.getSchoolOnboardPage().VerifySuccessMessageInSuperAdmin();
+//		pom.getSchoolOnboardPage().VerifySuccessMessageInSuperAdmin();
 
 	}
 
@@ -55,17 +55,17 @@ public class TC3_SchoolOnboardStep extends BaseClass {
 	public void theUserFetchAndFromMailosaurPortal(String string, String string2)
 			throws IOException, MailosaurException {
 
-		pom.getSchoolOnboardPage().FetchUsernameAndPasswordFromMailosaur();
+//		pom.getSchoolOnboardPage().FetchUsernameAndPasswordFromMailosaur();
 	}
 
 	@Then("the user logs in to the School Admin Dashboard using the mailed credentials")
 	public void theUserLogsInToTheSchoolAdminDashboardUsingTheMailedCredentials()
 			throws InterruptedException, FileNotFoundException, IOException {
 
-		driver.switchTo().newWindow(WindowType.TAB);
-		switchToNewTab1(driver);
+//		driver.switchTo().newWindow(WindowType.TAB);
+//		switchToNewTab1(driver);
 
-		Thread.sleep(3000);
+//		Thread.sleep(3000);
 
 //		enterUrl("https://abcd06.heycampus.in/school/login");
 
@@ -73,13 +73,13 @@ public class TC3_SchoolOnboardStep extends BaseClass {
 
 		pom.getSchoolOnboardPage().LogIntoSchoolAdminDashboard();
 
-		Thread.sleep(3000);
+//		Thread.sleep(3000);
 	}
 
 	@Then("the user should verify the School Admin Mail-ID on the Onboard page.")
 	public void theUserShouldVerifyTheSchoolAdminMailIDOnTheOnboardPage() throws FileNotFoundException, IOException {
 
-		pom.getSchoolOnboardPage().VerifySchoolNameAndMailIDOnOnboardPage();
+//		pom.getSchoolOnboardPage().VerifySchoolNameAndMailIDOnOnboardPage();
 
 	}
 
@@ -87,14 +87,14 @@ public class TC3_SchoolOnboardStep extends BaseClass {
 	public void theUserOnboardsTheSchoolUsingTheCorrespondingCSVFile()
 			throws FileNotFoundException, IOException, AWTException, InterruptedException, CsvException {
 
-		pom.getSchoolOnboardPage().OnboardSchoolUsingCSVFile();
+//		pom.getSchoolOnboardPage().OnboardSchoolUsingCSVFile();
 
 	}
 
 	@Then("the user should verify the {string} popup message in the School Admin Dashboard.")
 	public void theUserShouldVerifyThePopupMessageInTheSchoolAdminDashboard(String string) {
 
-		pom.getSchoolOnboardPage().SuccessfullOnboardPopup();
+//		pom.getSchoolOnboardPage().SuccessfullOnboardPopup();
 
 	}
 
